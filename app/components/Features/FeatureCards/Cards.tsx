7296e46437df41d1694d04b289d90df0fbf81bfa
T-Letuka <tisetsoletuka@gmail.com>
2024-03-31 19:15:11 +0200

    features section added

---
+++ app/components/Features/FeatureCards/Cards.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+interface Props {
+  title: string;
+  image: string;
+}
+
+const Cards = ({ title, image }: Props) => {
+  return (
+    <div
+      className="text-center bg-[#000814] p-4  rounded-lg hover:shadow-lg transition-all duration-300
+  cursor-pointer hover:scale-110"
+    >
+      <Image
+        src={image}
+        alt="icon"
+        width={70}
+        height={70}
+        className="mx-auto"
+      />
+      <h1 className="text-[20px] mt-[1.4rem] font-[500] text-white ">
+        {title}
+      </h1>
+      <p className="mt-[1rem] text-white opacity-90 text-[15px]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        similique eos architecto, distinctio quia provident facere corporis
+        commodi minus, at dolores et quam fugiat ad! A eaque tempora beatae
+        mollitia?
+      </p>
+      <p className="mt-[1.4rem] text-[#90E0EF] font-[500] cursor-pointer hover:text-red-600">
+        {" "}
+        Learn More
+      </p>
+    </div>
+  );
+};
+
+export default Cards;
--- app/components/Features/Features.tsx
+++ app/components/Features/Features.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import SectionHeading from "../SectionHeading/SectionHeading";
+import Cards from "./FeatureCards/Cards";
+import learning from "@/public/learning.png";
+import Quiz from "@/public/Quiz.png";
+import studyt from "@/public/studyt.png";
 
 const Features = () => {
   return (
@@ -8,6 +12,17 @@ const Features = () => {
         headingMini="Personalized Learning"
         headingPrimary="Transforming Study Habits Everywhere"
       />
+      <div className="pt-[5rem] w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-[1.4rem]">
+        <div>
+          <Cards title="Study Techniques" image={studyt.src} />
+        </div>
+        <div>
+          <Cards title="Quiz" image={Quiz.src} />
+        </div>
+        <div>
+          <Cards title="Learning Styles" image={learning.src} />
+        </div>
+      </div>
     </div>
   );
 };
