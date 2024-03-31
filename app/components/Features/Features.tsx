c1551bdd6843f884f9230e528f5bd1c4ebaa901b
T-Letuka <tisetsoletuka@gmail.com>
2024-03-31 18:14:37 +0200

    designed section heading

---
+++ app/components/Features/Features.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionHeading from "../SectionHeading/SectionHeading";
+
+const Features = () => {
+  return (
+    <div className="pt-[5rem] pb-[3rem]">
+      <SectionHeading
+        headingMini="Personalized Learning"
+        headingPrimary="Transforming Study Habits Everywhere"
+      />
+    </div>
+  );
+};
+
+export default Features;
